3781f2893aed90d91106097fd61edf65b9413d9d
Agah Özdemir <72024578+aagahoz@users.noreply.github.com>
2021-06-04 17:25:32 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,40 @@
 
 int main() 
 {
+	Ogrenci *ogrenciArray = dosyadanOgrenciOku();
+	Asistan *asistanArray = dosyadanAsistanOku();
+	Sinif *sinifArray = dosyadanSinifOku();
 	
+	ogrenciAsistanVeSinifSiraDuzenleme(asistanArray, sinifArray);
+
+//	ogrenciInfo(ogrenciArray);
+//	asistanInfo(asistanArray);
+//	sinifInfo(sinifArray);
+
+//	ogrenciEkle(ogrenciArray);
+//	asistanEkle(asistanArray);
+//	sinifEkle(sinifArray);
+
+//	ogrenciCikar(ogrenciArray);
+//	asistanCikar(asistanArray);
+//	sinifCikar(sinifArray);
+
+//	cout << ogrenciSayisi(ogrenciArray);
+//	cout << asistanSayisi(asistanArray);
+//	cout << sinifSayisi(sinifArray);
 	
-	
-	
+//	dosyaVeriGuncelleme(ogrenciArray, asistanArray, sinifArray);
+
+
+//	enAzSayidaAsistanaAyirma(ogrenciArray, asistanArray);
+//	asistanlaraEsitAyirma(ogrenciArray, asistanArray);
+
+//	siniflaraEsitAyirma(ogrenciArray, sinifArray);
+//	enAzSayidaSinifaAyirma(ogrenciArray, sinifArray);
+
+
+//	asistanOgrenciListesiDosyalariniOlustur(ogrenciArra);
+//	sinifOgrenciListesiDosyalariniOlustur(sinifArray);
 	
 	
 	return 0;
