2f7414a3e2f14d8d57c5faf3d1ff81cd2a6447ee
Agah Özdemir <72024578+aagahoz@users.noreply.github.com>
2021-06-04 17:25:17 +0300

    Update library.h

--- library.h
+++ library.h
@@ -2,28 +2,42 @@
 #define library_H_
 
 #include <iostream>
+#include <fstream>
 #include "ogrenci.h"
 #include "asistan.h"
 #include "sinif.h"
 using namespace std;
 
-void DosyadanOku(string dosyaAdi);
-void OgrenciInfo();//parametreleri gözden geçir
-void AsistanInfo();
-void SinifInfo();
-void OgrenciEkle();
-void AsistanEkle();
-void SinifEkle();
-void OgrenciCikar();
-void AsistanCikar();
-void SinifCikar();
-void AsitanlaraGoreAyirma();
-void AsistanlarEsitAyirma();
-void EnAzSayidaAsistanaAyirma();
-void VeriGuncelleme();
-void SiniflaraAyirma();
-void SiniflaraEsitAyirma();
-void EnAzSayidaSinifaAyirma();
+Ogrenci *dosyadanOgrenciOku();
+Asistan *dosyadanAsistanOku();
+Sinif *dosyadanSinifOku();
 
+void ogrenciInfo(Ogrenci *ogrenciArray);
+void asistanInfo(Asistan *asistanArray);
+void sinifInfo(Sinif *sinifArray);
+
+int ogrenciSayisi(Ogrenci *ogrenciArray);
+int asistanSayisi(Asistan *asistanArray);
+int sinifSayisi(Sinif *sinifArray);
+
+void ogrenciEkle(Ogrenci *ogrenciArray);
+void asistanEkle(Asistan *asistanArray);
+void sinifEkle(Sinif *sinifArray);
+
+//void ogrenciCikar(Ogrenci *ogrenciArray);
+//void asistanCikar(Asistan *asistanArray);
+//void sinifCikar(Sinif *sinifArray);
+
+void asistanlaraEsitAyirma(Ogrenci *ogrenciArray, Asistan *asistanArray);
+void enAzSayidaAsistanaAyirma(Ogrenci *ogrenciArray, Asistan *asistanArray);
+void siniflaraEsitAyirma(Ogrenci *ogrenciArray, Sinif *sinifArray);
+void enAzSayidaSinifaAyirma(Ogrenci *ogrenciArray, Sinif *sinifArray);
+
+void dosyaVeriGuncelleme(Ogrenci *ogrenciArray, Asistan *asistanArray, Sinif *sinifArray);
+void ogrenciAsistanVeSinifSiraDuzenleme(Asistan *asistanArray, Sinif *sinifArray);
+
+void asistanOgrenciListesiDosyalariniOlustur(Asistan *asistanArray);
+void sinifOgrenciListesiDosyalariniOlustur(Sinif *sinifArray);
+void ogrenciListesiniDosyayaYaz(string dosyaAdi, Ogrenci* ogrenciArray);
 
 #endif
